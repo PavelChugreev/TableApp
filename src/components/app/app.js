b6156a4751dd8102eebc040ff3591e34d312fef2
PavelChugreev <chugun92@gmail.com>
2020-12-19 16:51:36 +0300

    change request link

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,7 +19,7 @@ export default class App extends Component {
         this.searchRows = this.searchRows.bind(this);
         this.addItem = this.addItem.bind(this);
 
-        this.getResourse("http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D")
+        this.getResourse("https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D")
             .then(response => {
                 this.setState({ data: response })
             })
