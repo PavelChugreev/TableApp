cd13f0072f7f754cde84e58515ad602a8378c868
PavelChugreev <chugun92@gmail.com>
2021-01-12 22:28:14 +0300

    add alternative method to get data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import SearchPanel from "../search-panel/search-panel";
 import AddForm from "../add-form/add-form";
 import Table from "../table/table";
+import data from "../app/data";
 import "./app.css";
 
 export default class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: [],
+            data: data,
+            // data: [],
             sorted: "no",
             inputSearchValue: "",
         }
@@ -19,10 +21,10 @@ export default class App extends Component {
         this.searchRows = this.searchRows.bind(this);
         this.addItem = this.addItem.bind(this);
 
-        this.getResourse("https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D")
-            .then(response => {
-                this.setState({ data: response })
-            })
+        // this.getResourse("https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D")
+        //     .then(response => {
+        //         this.setState({ data: response })
+        //     })
     }
 
     async getResourse(url) {
